d23390774222d84c7119de47be3230cacb02d2e1
syq <shenyongqiang2008@126.com>
2017-05-15 14:33:16 +0800

    更新版本号

--- SetPanel/XKGameVersionCtrl.cs
+++ SetPanel/XKGameVersionCtrl.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class XKGameVersionCtrl : MonoBehaviour {
 	UILabel VersionLB;
-	public static string GameVersion = "Version: V1.1_20170330";
+	/**
+	 * V1.2_20170515战火雄心-坦克大战:
+	 * 1.处理第四关玩家合体时,如果游戏进入倒计时,画面会停止运动的bug.
+	 */
+	public static string GameVersion = "Version: V1.2_20170515";
 	// Use this for initialization
 	void Start()
 	{
